6ed8a7c2bd60ec5e5d7aa32cd189671a4fc93eb6
SMilanchev <48320090+SMilanchev@users.noreply.github.com>
2024-10-24 12:35:37 +0300

    added preline with sipmle element for test

--- README.md
+++ README.md
@@ -13,8 +13,13 @@ To serve locally, run `npm run serve --port=[PORT]`, and then go to Weweb editor
 ## Build
 
 Before release, you can check build error by running `npm run build --name=my-element`
+## npm run build -- name=my-element type=element
 
 
 
-## Here I will write all the steps for creating component working in weweb.
+## Here I will write all the steps for creating component with preline working in weweb.
 1. `npm init @weweb/component`
+2. Add preline: [Preline Documentation](https://www.preline.co/docs/index.html)
+ - install tailwind [Tailwind Installation](https://tailwindcss.com/docs/installation)
+ - then install preline
+
--- src/wwElement.vue
+++ src/wwElement.vue
@@ -2,9 +2,36 @@
   <div class="my-element">
     <p :style="textStyle">I am a custom element !</p>
   </div>
+
+  <div class="flex space-x-4">
+  <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 focus:outline-none focus:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none dark:bg-white dark:text-neutral-800">
+  Button
+</button>
+<button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-gray-500 text-white hover:bg-gray-600 focus:outline-none focus:bg-gray-600 disabled:opacity-50 disabled:pointer-events-none">
+  Button
+</button>
+<button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-teal-500 text-white hover:bg-teal-600 focus:outline-none focus:bg-teal-600 disabled:opacity-50 disabled:pointer-events-none">
+  Button
+</button>
+<button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+  Button
+</button>
+<button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:bg-red-600 disabled:opacity-50 disabled:pointer-events-none">
+  Button
+</button>
+<button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-yellow-500 text-white hover:bg-yellow-600 focus:outline-none focus:bg-yellow-600 disabled:opacity-50 disabled:pointer-events-none">
+  Button
+</button> 
+<button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-white text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none">
+  Button
+</button>
+</div>
 </template>
 
+
+
 <script>
+import "./output.css";
 export default {
   props: {
     content: { type: Object, required: true },
@@ -18,6 +45,8 @@ export default {
   },
 };
 </script>
+
+
 
 <style lang="scss" scoped>
 .my-element {
